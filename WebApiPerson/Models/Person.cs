9c33c377da18e7c31f8329e1800088596efb5641
Soria Yumbo Jesica Yadira <jsoria8559@uta.edu.ec>
2025-04-26 23:14:55 -0500

    Agregar archivos de proyecto.

---
+++ WebApiPerson/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiPerson.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public required string NAME { get; set; }
+
+        public required int AGE {  get; set; }
+    }
+}
